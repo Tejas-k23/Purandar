ea984c1fdbe90af11e4d6f9434881761ddb62b46
Tejas  Kumbharkar <tejaskumbharkar6398@gmail.com>
2026-04-30 16:32:01 +0530

    payment 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const startServer = async () => {
   }
 
   // ✅ ALWAYS start server
-  app.listen(env.PORT || 5000, () => {
+  app.listen(env.PORT || 5000, '0.0.0.0', () => {
     console.log(`Backend running on port ${env.PORT}`);
   });
 };
